135e33623e0396a206193eebd12adf919937e256
Steven Tey <stevensteel97@gmail.com>
2023-04-02 13:47:00 -0500

    fixed mobile styles, added a nice mobile menu

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -24,7 +24,7 @@ export default function Container({ children }: { children: ReactNode }) {
   const segment = useSelectedLayoutSegments();
 
   return (
-    <main className="mx-auto max-w-2xl lg:max-w-5xl mt-16 sm:mt-32">
+    <main className="mx-5 sm:mx-auto max-w-2xl lg:max-w-5xl mt-16 sm:mt-32">
       {segment.length === 1 && (
         <div className="max-w-2xl">
           <h1 className="text-4xl font-display tracking-tight text-zinc-800 sm:text-5xl">
