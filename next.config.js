46e802e806da239eb209ed9d266fe21b84ce6947
Steven Tey <stevensteel97@gmail.com>
2023-05-18 10:47:43 -0500

    update next to latest, added stuff to article

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [
       "images.unsplash.com",
